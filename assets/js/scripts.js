ee4460e656eb1187d5c27c5d7cbb560e1cedf268
spf34 <samuel.forster@cantab.net>
2021-01-31 21:37:42 +0000

    add shuffleCards methods

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,21 @@ function turnCard(){
     this.classList.toggle('turned');
 }
 
-for(i = 0; i < cards.length; i++){
-    card = cards[i];
-    cards[i] = card.addEventListener('click', turnCard);    
+// Durstenfeld shuffle
+function shuffleCards(cards){
+    for(let i = cards.length - 1; i > 0; i--){
+        // given index i, take random index j < i
+        let j = Math.floor(Math.random() * (i + 1));
+            // interchange orderings of the ith and jth cards
+            cards[j].style.order = i;
+            cards[i].style.order = j;
+    }
 }
+
+shuffleCards(cards);
+
+for(let i = 0; i < cards.length; i++){
+    card = cards[i];
+    cards[i] = card.addEventListener('click', turnCard)
+}
+
